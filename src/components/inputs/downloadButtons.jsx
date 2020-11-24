b541e888d8ff48dc7f8473a6dd0a96dabe106800
Tilman Miraß <mail@tlmn.io>
2020-11-24 11:54:28 +0100

    add download button component

---
+++ src/components/inputs/downloadButtons.jsx
@@ -0,0 +1,39 @@
+import IconDownload from "../../assets/svg/download";
+import React from "react";
+import { html2image } from "../../lib/lib";
+
+export default ({ state, setState, propertyPath }) => (
+  <div className="flex mb-2" style={{ gap: "10px" }}>
+    <button
+      className="btn btn-download flex justify-center"
+      onClick={() =>
+        html2image(
+          {
+            state,
+            setState,
+          },
+          propertyPath,
+          "svg"
+        )
+      }
+    >
+      <IconDownload height={20} className="mr-1" /> SVG
+    </button>
+
+    <button
+      className="btn btn-download flex justify-center"
+      onClick={() =>
+        html2image(
+          {
+            state,
+            setState,
+          },
+          propertyPath,
+          "png"
+        )
+      }
+    >
+      <IconDownload height={20} className="mr-1" /> PNG
+    </button>
+  </div>
+);
