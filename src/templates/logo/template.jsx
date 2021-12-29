f86b07b87c852d117b9fb14315092ba9aa0006d3
tlmn <mail@tlmn.io>
2021-12-29 11:14:19 +0100

    deconstruct

--- src/templates/logo/template.jsx
+++ src/templates/logo/template.jsx
@@ -13,6 +13,11 @@ import useDataContext from "../../lib/useDataContext";
 
 const Template = () => {
   const { state } = useDataContext();
+  const {
+    data: { scales, logoVariant, localBranch },
+    templateScale,
+    refs,
+  } = state;
   return (
     <div className="col-span-6 relative grid-cols-2 grid gap-x-2">
       <div className="col-span-1" style={{ height: "360px" }}>
@@ -27,16 +32,16 @@ const Template = () => {
           }}
           ref={state.refs[0]}
         >
-          {renderLogoTextRight(state.data.logoVariant, 473, getColor(state, 0))}
+          {renderLogoTextRight(logoVariant, 473, getColor(state, 0))}
           <span
             className="absolute top-0 left-0 text-md font-headline italic uppercase leading-none break-all"
             style={{
               color: getColor(state, 0),
               ...getTextStyles(state, 0),
-              fontSize: `${state.data.scales[0].value}px`,
+              fontSize: `${scales[0].value}px`,
             }}
             dangerouslySetInnerHTML={{
-              __html: state.data.localBranch.content.replace(/\n/gi, "<br/>"),
+              __html: localBranch.content.replace(/\n/gi, "<br/>"),
             }}
           />
         </div>
@@ -46,27 +51,23 @@ const Template = () => {
         <DownloadButtons propertyPath="refs[1]" />
         <div
           className={`p-4 relative // flex flex-col items-center justify-center  // border-1 // template ${
-            state.templateScale ? `template-scale m-auto` : `relative`
+            templateScale ? `template-scale m-auto` : `relative`
           }`}
           style={{
             backgroundColor: "transparent",
           }}
-          ref={state.refs[1]}
+          ref={refs[1]}
         >
-          {renderLogoTextBottom(
-            state.data.logoVariant,
-            473,
-            getColor(state, 0)
-          )}
+          {renderLogoTextBottom(logoVariant, 473, getColor(state, 0))}
           <span
             className="absolute text-center top-0 text-md font-headline italic uppercase leading-none"
             style={{
               color: getColor(state, 0),
               ...getTextStyles(state, 1),
-              fontSize: `${state.data.scales[1].value}px`,
+              fontSize: `${scales[1].value}px`,
             }}
             dangerouslySetInnerHTML={{
-              __html: state.data.localBranch.content.replace(/\n/gi, "<br/>"),
+              __html: localBranch.content.replace(/\n/gi, "<br/>"),
             }}
           />
         </div>
@@ -76,12 +77,12 @@ const Template = () => {
         <DownloadButtons propertyPath="refs[2]" />
         <div
           className={`p-4 relative // flex flex-col items-center justify-center // border-1 // template ${
-            state.templateScale ? `template-scale` : `relative`
+            templateScale ? `template-scale` : `relative`
           }`}
           style={{
             backgroundColor: "transparent",
           }}
-          ref={state.refs[2]}
+          ref={refs[2]}
         >
           <LogoArrow fillColor={getColor(state, 0)} width={250} />
           <span
@@ -89,10 +90,10 @@ const Template = () => {
             style={{
               color: getColor(state, 0),
               ...getTextStyles(state, 2),
-              fontSize: `${state.data.scales[2].value}px`,
+              fontSize: `${scales[2].value}px`,
             }}
             dangerouslySetInnerHTML={{
-              __html: state.data.localBranch.content.replace(/\n/gi, "<br/>"),
+              __html: localBranch.content.replace(/\n/gi, "<br/>"),
             }}
           />
         </div>
@@ -101,7 +102,7 @@ const Template = () => {
         <DownloadButtons propertyPath="refs[3]" />
         <div
           className={`p-4 relative // flex flex-col items-center justify-center  // border-1 // template ${
-            state.templateScale ? `template-scale` : `relative`
+            templateScale ? `template-scale` : `relative`
           }`}
           style={{
             backgroundColor: "transparent",
