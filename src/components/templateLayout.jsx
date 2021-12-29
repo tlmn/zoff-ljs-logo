d033881e06a1096d68172e6278e4f5cce114bc55
tlmn <mail@tlmn.io>
2021-12-29 11:32:04 +0100

    add screenshot effect

--- src/components/templateLayout.jsx
+++ src/components/templateLayout.jsx
@@ -1,10 +1,19 @@
 import Layout from "../components/layout";
 import React from "react";
+import useDataContext from "../lib/useDataContext";
 
-const TemplateLayout = ({ children }) => (
-  <Layout>
-    <div className="container grid-12 py-2">{children}</div>
-  </Layout>
-);
+const TemplateLayout = ({ children }) => {
+  const {
+    state: { templateScale },
+  } = useDataContext();
+  return (
+    <Layout>
+      {!templateScale && (
+        <div className="fixed bg-white h-screen w-screen z-50"></div>
+      )}
+      <div className="container grid-12 py-2">{children}</div>
+    </Layout>
+  );
+};
 
 export default TemplateLayout;
